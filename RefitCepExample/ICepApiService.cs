dc959dfee3500c5d53dfd1c0374af2ce987ac339
Paulo Vilan <paulovilan@gmail.com>
2021-07-05 14:34:47 -0300

    Adicionar arquivos de projeto.

---
+++ RefitCepExample/ICepApiService.cs
@@ -0,0 +1,15 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefitCepExample
+{
+	public interface ICepApiService
+	{
+		[Get("/ws/{cep}/json")]
+		Task<CepResponse> GetAddressAsync(string cep);
+	}
+}
---
+++ RefitCepExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace RefitCepExample
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var cepClient = RestService.For<ICepApiService>("https://viacep.com.br");
+			string meuCep = "13300390";
+			Console.WriteLine("Consultando dados do Cep:" + meuCep);
+
+
+			var address = await cepClient.GetAddressAsync(meuCep);
+
+
+			Console.Write($"\nLogradouro:{address.Logradouro}\nBairro:{address.Bairro}" +
+				$"\nEstado:{address.Uf}\nCódigo Ibge:{address.Ibge}");
+			Console.ReadKey();
+		}
+	}
+}
